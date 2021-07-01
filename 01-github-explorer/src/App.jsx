9861c48ad206cb2f7da57c8662cbf69e5b027342
vini <viniciusnunes1709@gmail.com>
2021-06-30 23:42:10 -0300

    Final modulo: Configurando ambiente

--- 01-github-explorer/src/App.jsx
+++ 01-github-explorer/src/App.jsx
@@ -1,3 +1,5 @@
+import './styles/global.scss';
+
 export function App() {
-    return <h1>Hello World</h1>
+    return <h1>Hello ReactJs</h1>
 }
--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+	mode: 'development',
+	devtool: 'eval-source-map',
 	entry: path.resolve(__dirname, 'src', 'index.jsx'),
 	output: {
 		path: path.resolve(__dirname, 'dist'),
@@ -9,13 +12,26 @@ module.exports = {
 	resolve: {
 		extensions: ['.js', '.jsx'],
 	},
+	devServer: {
+		contentBase: path.resolve(__dirname, 'public'),
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, 'public', 'index.html')
+		})
+	],
 	module: {
 		rules: [
 			{
 				test: /\.jsx$/,
 				exclude: /node_modules/,
 				use: 'babel-loader',
-			}
+			},			
+			{
+				test: /\.scss$/,
+				exclude: /node_modules/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
 		]
 	}
 }
